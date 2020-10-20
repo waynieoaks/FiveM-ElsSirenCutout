c5904c6c58460a5514c6ee1f41194442d02919ec
Waynieoaks <5962977+waynieoaks@users.noreply.github.com>
2020-10-20 23:00:24 +0100

    Initial coding
    Can detect that code 3 lighting is on, just need to triger the "stop command"

---
+++ SirenCutout/SirenCutout/bin/Debug/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Waynieoaks'
+description 'Stop ELS Sirens on exiting vehicle'
+version '0.1.1'
+
+client_script 'SirenCutout.net.dll'
---
+++ SirenCutout/SirenCutout/main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CitizenFX;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+
+namespace SirenCutout
+{
+    public class Main : BaseScript
+    {
+        // Declare any variables here
+        public Main()
+        {           
+           base.Tick += OnTick;
+        }
+
+       public async Task OnTick()
+        {
+            var ped = GetPlayerPed(-1);
+
+            if (
+                DoesEntityExist(ped) &&
+                IsPedInAnyVehicle(ped, false) &&
+                !IsEntityDead(ped) &&
+                !IsPauseMenuActive() &&
+                IsControlJustPressed(2, 75)
+                )
+            {
+                var vehicle = GetVehiclePedIsIn(ped, false);
+
+                if (IsVehicleSirenOn(vehicle))
+                {
+                    // Send it 4 times, for some reason... 
+                    for (int index = 0; index < 4; ++index)
+                    {
+                        // SendKeys.Send("J");
+                        //SetVehicleSiren(vehicle, false);
+                    }
+
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        args = new[] { "[SirenCutout]", $"You left your siren on, idiot!" }
+                    });
+                }
+            }
+
+            await Task.FromResult(1);
+        }
+
+    }
+}
